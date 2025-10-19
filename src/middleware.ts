6147b701b0e77367f1c417a7073134d65c20a338
animesh <phb21013@tezu.ac.in>
2025-10-20 00:57:04 +0530

    middlewere is heavy but how

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,42 +7,29 @@ export async function middleware(req: NextRequest) {
     const session = (await auth())?.user?.email;
 
     const { nextUrl } = req;
-    // next url is a whole long url object having href, pathname port etc
-    // console.log(nextUrl);
+  
 
     const isApiAuthRoutes = nextUrl.pathname.startsWith(authapiPrefix);
 
-    // console.log(isApiAuthRoutes);
-    // const isPublicRoutes = publicRoutes.includes(nextUrl.pathname)
     const isPublicRoutes = publicRoutes.some((route) => {
         if (route.endsWith("/*")) {
-            // this is for submit thingy => like if the route ends with /asdadsa => then it slice it
-            const baseUrl = route.slice(0, -2); // making /submit
-            return nextUrl.pathname.startsWith(baseUrl + "/"); // then add / and checks the real routes
+          
+            const baseUrl = route.slice(0, -2); 
+            return nextUrl.pathname.startsWith(baseUrl + "/"); 
         }
         return nextUrl.pathname === route;
     });
 
-    // console.log(isPublicRoutes, "public", nextUrl.pathname);
 
     const isAuthenticationRoutes = authenticationRoutes.includes(
         nextUrl.pathname
     );
 
-    // console.log("auth", isAuthenticationRoutes);
 
-    /**
-     * user try to visit /api/auth for signin from anything
-     * let them be in
-     */
     if (isApiAuthRoutes) {
         return;
     }
 
-    /**
-     * if user goes to /signup or /login page let them be in
-     * if already logged in them redirect to default_redirect_url
-     */
     if (isAuthenticationRoutes) {
         if (session) {
             return Response.redirect(new URL(DEFAULT_REDIRECT_URL, nextUrl));
